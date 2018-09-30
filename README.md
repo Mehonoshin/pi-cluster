ee2ea32c69959ea6f1da5116b664cbc566ca776a
Stanislav Mekhonoshin <ejabberd@gmail.com>
2018-09-30 20:07:20 +0300

    Install postgresql on zabbix-server node

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 ### Installation
 
 Install sshpass to run the first time with password `https://github.com/hudochenkov/homebrew-sshpass`
+
+Install external roles with command: `ansible-galaxy install -r requirements.yml`
--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,22 @@
 #!/bin/sh
-ansible-playbook -i inventory playbooks/node.yml
+
+usage()
+{
+	echo "Usage help"
+}
+
+verbose=""
+
+while [ "$1" != "" ]; do
+	case $1 in
+			-d | --debug )          shift
+															verbose="-$1"
+															;;
+			-h | --help )           usage
+															exit
+															;;
+	esac
+	shift
+done
+
+ansible-playbook $verbose -i inventory playbooks/node.yml
